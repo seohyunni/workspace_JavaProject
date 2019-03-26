cdc9fdcb6db59e10518797bd5f563ff36943f986
seohyunni <miku0331@naver.com>
2019-03-26 19:49:00 +0900

    add Echo project

--- 201903/src/kr/hs/emirim/page/Echo_20190326.java
+++ 201903/src/kr/hs/emirim/page/Echo_20190326.java
@@ -1,5 +1,18 @@
 package kr.hs.emirim.page;
-
+import java.util.*;
 public class Echo_20190326 {
-
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+			System.out.print("아무말 >> ");
+			String answer=sc.nextLine();
+			if(answer.equals("exit")) {
+				System.out.println("끝");
+				break;
+			}
+			
+			System.out.println(answer);
+			
+		}
+	}
 }
--- 201903/src/kr/hs/emirim/page/page.java
+++ 201903/src/kr/hs/emirim/page/page.java
@@ -21,18 +21,24 @@ public class page {
 	System.out.println(결과+" 페이지가 필요합니다.");
 	}*/
 	while(true) {
+		//input
 		int nPerPage=7;	
 		Scanner sc=new Scanner(System.in);
 		System.out.println("몇 개의 게시물을 표시할까요?(정수로 쓰세요.)");
-		int result=sc.nextInt();
-		if(result==0) {
+		String inputString = sc.nextLine();
+		//if exit? -> exit
+		if(inputString.equals("exit")) {
 			System.out.println("끝");
 			break;
 		}
+		//String -> int
+		int result = Integer.parseInt(inputString);
+		//calculate page
 		int 결과 = result/nPerPage;
 		if(result%nPerPage!=0) {
 			결과 = result/nPerPage +1;
 		}
+		//output
 	
 				System.out.println(결과+" 페이지가 필요합니다.");
 	}
