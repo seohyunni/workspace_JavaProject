a310db424e74d3b7da97961adba49ac50cc79461
seohyunni <miku0331@naver.com>
2019-04-02 19:27:20 +0900

    커밋 메시지

--- 201903/src/kr/hs/emirim/page/UpDownGameBot.java
+++ 201903/src/kr/hs/emirim/page/UpDownGameBot.java
@@ -5,30 +5,29 @@ import java.util.Scanner;
 
 public class UpDownGameBot {
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		
-		System.out.println("숫자를 생각해 주세요.");
-		
-		
-		
-		while(true){
-			Random r=new Random();
-			int num=r.nextInt(100);
-			
-			
-			System.out.print(num+" UP? or DOWN? or RIGHT? >> ");
-			
-			String answer=sc.nextLine();
-			if(answer.equals("UP")) {
-				num=-num;
-			}
-			if(answer.equals("DOWN")) {
-				num=+num;
-			}
-			if(answer.equals("RIGHT")) {
-				System.out.println("저의 승리입니다.");
-			}
-		}
-			
+		int start =0;
+		int end = 99;
+		  Scanner sc=new Scanner(System.in);
+		  
+		  Random random = new Random();
+		  while(true) {
+		  int rNumber = random.nextInt(end+1-start)+start; 
+		  System.out.println(rNumber+ " 입니까?");
+		  
+			  String answer = sc.nextLine();
+			  if(answer.equals("down")) {
+				  end=rNumber-1;
+			  }
+			  
+			  else if(answer.equals("up")) {
+				  start=rNumber+1;
+				  
+			  }
+			  
+			  else if(answer.equals("정답")) {
+				 System.out.println("ㅋㅋ"); break;
+			 }
+				  
+		  }
 	}
 }
