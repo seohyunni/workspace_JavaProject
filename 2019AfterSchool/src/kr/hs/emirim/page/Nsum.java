89828e151cabc06bf8fdcb41d3ec05c222652fdc
seohyunni <miku0331@naver.com>
2019-04-04 19:56:48 +0900

    sum

---
+++ 2019AfterSchool/src/kr/hs/emirim/page/Nsum.java
@@ -0,0 +1,24 @@
+package kr.hs.emirim.page;
+
+import java.util.Scanner;
+
+public class Nsum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//숫자 입력받자
+		//각 자릿수의 수를 더하자
+		//더한 값을 출력자자
+		int sum=0;
+		System.out.print("숫자 입력 >> ");
+		String n=sc.nextLine();
+		
+		int length = n.length();
+		for(int i=0; i<length; i++) {
+			 char num = n.charAt(i);
+			sum += num-'0';
+		}
+		System.out.println(sum);
+	}
+
+}
